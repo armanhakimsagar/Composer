84a4a78b853bb844e003d2eca65fabeb30eda413
arman hakim sagar <arman.sagar@dent.org.bd>
2018-10-01 13:10:30 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+ require 'vendor/autoload.php';
+ 
+ $Animal = new Base\Animal;
+ $Animal->myClass();
+
+?>
---
+++ src/Animal.php
@@ -0,0 +1,10 @@
+<?php
+  namespace Base;
+  
+  class Animal{
+	public function myClass(){
+		echo "Hello class found";
+	}
+  }
+
+?>
